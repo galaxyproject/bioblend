f836a81aaa34235044e0cbe067d973cc2d3f21c4
Gregory Long <gregorydavidlong@gmail.com>
2012-06-05 17:20:41 +1000

    Modified structure of tests to be more readable.

--- tests/cloudman_tests.py
+++ tests/cloudman_tests.py
@@ -3,78 +3,60 @@ from mock import MagicMock
 from blend import cloudman
 
 # Some ad-hoc tests for the CloudMan API
-# Expects CloudMan to be running locally
 # This is fairly unreadable at the moment. This needs to be fixed.
 
 class CloudManTest(unittest.TestCase):
 
-    def test_basic_usage(self):
-        ## Start master
-        ##cluster = cloudman.StartCluster
-        ##cluster.ip
-        
+    def setUp(self):
         url = "http://127.0.0.1:42284"
         password = "password"
+        self.cm = cloudman.CloudMan(url, password)
         
-        cm = cloudman.CloudMan(url, password)
-        self.assertIsNotNone(cm)
+
+    def test_basic_usage_against_local_instance(self):
+        # Expects CloudMan to be running locally
+        
+        ## Start master
+        #cluster = cloudman.StartCluster
+        #cluster.ip
+        
+        self.assertIsNotNone(self.cm)
 
         ## Set cluster type and storage size
-        cm.initialize(type="xxx", storage_size="yyy")
+        self.cm.initialize(type="xxx", storage_size="yyy")
         
         ## Get cluster status
-        # Set return value of call
-        cm._make_get_request = MagicMock(return_value="{}")
-
-        status = cm.get_status()
+        status = self.cm.get_status()
         self.assertIsNotNone(status)
-        self.assertEquals(status, {})
-        
-        # Check that the correct URL was called
-        cm._make_get_request.assert_called_with("/cloud/instance_state_json")
         
         ## Get nodes
-        # Set return value of call
-        cm._make_get_request = MagicMock(return_value="{\"instances\": []}")
-
-        nodes = cm.get_nodes()
+        nodes = self.cm.get_nodes()
         self.assertIsNotNone(nodes)
-        self.assertEqual(len(nodes), 0)
+        # There should be a master node
+        self.assertEqual(len(nodes), 1)
 
-        # Check that the correct URL was called
-        cm._make_get_request.assert_called_with("/cloud/instance_feed_json")
-        
         ## Add node
         num_nodes = 10
-        status = cm.add_nodes(num_nodes, type="xxx", spot_price="yyy")
+        status = self.cm.add_nodes(num_nodes, type="xxx", spot_price="yyy")
         self.assertIsNotNone(status)
 
-        # Check that the correct URL was called
-        params = {'number_nodes': 10}
-        cm._make_get_request.assert_called_with("/cloud/add_instances", parameters=params)
-        
         ## Remove nodes
         instance_id = "abcdef"
-        status = cm.remove_nodes(num_nodes, force=True)
+        status = self.cm.remove_nodes(num_nodes, force=True)
         self.assertIsNotNone(status)
 
-
-        # Check that the correct URL was called
-        params = {'number_nodes': 10, 'force_termination': True}
-        cm._make_get_request.assert_called_with("/cloud/remove_instances", parameters=params)
-        
-        cm.remove_node(instance_id, force=True)
+        self.cm.remove_node(instance_id, force=True)
 
         ## Reboot instance
-        cm.reboot_node(instance_id)
+        self.cm.reboot_node(instance_id)
         
         ## Autoscaling
-        cm.enable_autoscaling(minimum_nodes=0,maximum_nodes=19) #20 is max on AWS
-        cm.disable_autoscaling()
-        cm.adjust_autoscaling(minimum_nodes=None,maximum_nodes=None)
+        self.cm.enable_autoscaling(minimum_nodes=0,maximum_nodes=19) #20 is max on AWS
+        self.cm.disable_autoscaling()
+        self.cm.adjust_autoscaling(minimum_nodes=None,maximum_nodes=None)
         
         ## Get Galaxy DNS/Host
-        galaxy_state = cm.get_galaxy_state() #RUNNING, STARTING.....
+        galaxy_state = self.cm.get_galaxy_state() #RUNNING, STARTING.....
         
         ## Restart cluster
         
@@ -84,3 +66,80 @@ class CloudManTest(unittest.TestCase):
         ## update galaxy
 
 
+    
+    def test_initialize(self):
+        self.assertIsNotNone(self.cm)
+
+        ## Set cluster type and storage size
+        self.cm.initialize(type="xxx", storage_size="yyy")
+
+
+    def test_get_status(self):
+        # Set return value of call
+        self.cm._make_get_request = MagicMock(return_value="{}")
+
+        status = self.cm.get_status()
+        self.assertIsNotNone(status)
+        self.assertEquals(status, {})
+        
+        # Check that the correct URL was called
+        self.cm._make_get_request.assert_called_with("/cloud/instance_state_json")
+       
+
+    def test_get_nodes(self):
+        # Set return value of call
+        self.cm._make_get_request = MagicMock(return_value="{\"instances\": []}")
+
+        nodes = self.cm.get_nodes()
+        self.assertIsNotNone(nodes)
+        self.assertEqual(len(nodes), 0)
+
+        # Check that the correct URL was called
+        self.cm._make_get_request.assert_called_with("/cloud/instance_feed_json")
+        
+
+    def test_add_nodes(self):
+        self.cm._make_get_request = MagicMock(return_value="{}")
+        num_nodes = 10
+        status = self.cm.add_nodes(num_nodes, type="xxx", spot_price="yyy")
+        self.assertIsNotNone(status)
+
+        # Check that the correct URL was called
+        params = {'number_nodes': 10}
+        self.cm._make_get_request.assert_called_with("/cloud/add_instances", parameters=params)
+      
+
+    def test_remove_nodes(self):
+        self.cm._make_get_request = MagicMock(return_value="{}")
+        num_nodes = 10
+        status = self.cm.remove_nodes(num_nodes, force=True)
+        self.assertIsNotNone(status)
+
+        # Check that the correct URL was called
+        params = {'number_nodes': 10, 'force_termination': True}
+        self.cm._make_get_request.assert_called_with("/cloud/remove_instances", parameters=params)
+        
+
+    def test_remove_node(self):
+        instance_id = "abcdef"
+        self.cm.remove_node(instance_id, force=True)
+        assert(False)
+
+
+    def test_reboot_node(self):
+        instance_id = "abcdef"
+        self.cm.reboot_node(instance_id)
+        assert(False)
+        
+
+    def test_autoscaling(self):
+        self.cm.enable_autoscaling(minimum_nodes=0,maximum_nodes=19) #20 is max on AWS
+        self.cm.disable_autoscaling()
+        self.cm.adjust_autoscaling(minimum_nodes=None,maximum_nodes=None)
+        assert(False)
+       
+
+    def test_get_galaxy_state(self):
+        galaxy_state = self.cm.get_galaxy_state() #RUNNING, STARTING.....
+        assert(False)
+        
