c7c136a490278dd8b26b61571ba8b6d5d8f12ce3
Gregory Long <gregorydavidlong@gmail.com>
2012-06-25 16:24:05 +1000

    Renamed create_cluster to launch. Created get_region function.

--- tests/bcc_tests.py
+++ tests/bcc_tests.py
@@ -1,14 +1,14 @@
 import unittest
 from mock import MagicMock
-from blend import biocloudcentral
+from blend import bcc
 import simplejson
 
-class BioCloudCentralTest(unittest.TestCase):
+class BCCTest(unittest.TestCase):
     def setUp(self):
         url = "http://127.0.0.1:8000"
-        self.bcc = biocloudcentral.BioCloudCentral(url)
+        self.bcc = bcc.BCC(url)
 
-    def test_get_cloud_types(self):
+    def test_get_clouds(self):
         cloud_hash = {
                 'id': '0',
                 'name': 'Test Cloud',
@@ -26,15 +26,15 @@ class BioCloudCentralTest(unittest.TestCase):
         }
         self.bcc._make_get_request = MagicMock(return_value=simplejson.dumps([cloud_hash]))
 
-        cloud_types = self.bcc.get_cloud_types()
-        self.bcc._make_get_request.assert_called_with("/api/get_cloud_types")
-        self.assertTrue(len(cloud_types) == 1)
+        clouds = self.bcc.get_clouds()
+        self.bcc._make_get_request.assert_called_with("/api/get_clouds")
+        self.assertTrue(len(clouds) == 1)
 
 
     def test_get_images(self):
         images_hash = {
                 'id': '0'
-                }
+        }
         self.bcc._make_get_request = MagicMock(return_value=simplejson.dumps([images_hash]))
 
         images = self.bcc.get_images()
@@ -45,7 +45,7 @@ class BioCloudCentralTest(unittest.TestCase):
     def test_get_instance_types(self):
         instance_hash = {
                 'id': '0'
-                }
+        }
         self.bcc._make_get_request = MagicMock(return_value=simplejson.dumps([instance_hash]))
 
         instances = self.bcc.get_instance_types()
@@ -53,33 +53,72 @@ class BioCloudCentralTest(unittest.TestCase):
         self.assertTrue(len(instances) == 1)
 
 
-    def test_create_cluster(self):
+    def test_get_regions(self):
+        region_hash = {
+                'name': 'a_region'
+        }
+        self.bcc._make_get_request = MagicMock(return_value=simplejson.dumps([region_hash]))
+
+        regions = self.bcc.get_regions('cloud_name', 'access_key', 'secret_key', 'instance_type')
+        params = {
+                'cloud_name': 'cloud_name',
+                'access_key': 'access_key',
+                'secret_key': 'secret_key',
+                'instance_type': 'instance_type'
+                }
+        self.bcc._make_get_request.assert_called_with("/api/get_regions", parameters=params)
+        self.assertTrue(len(regions) == 1)
+
+
+
+    def test_launch(self):
         cluster_name = "Test cluster"
         password = 'password'
-        cloud_type = '1'
+        cloud_name = 'Amazon'
         access_key = 'access_key'
         secret_key = 'secret_key'
-        instance_type = '2'
+        instance_type = 'm1.small'
         zone = 'zone'
         psscript = 'pss'
         wscript = 'ws'
-        image_id = '3'
+        image_id = 'ami-00000001'
 
         launch_hash = {
                 'cluster_name': cluster_name,
                 'password': password,
-                'cloud': cloud_type,
+                'cloud': cloud_name,
                 'access_key': access_key,
                 'secret_key': secret_key,
                 'instance_type': instance_type,
                 #'bucket_default': '',
-                'post_start_script_url': psscript,
+                #'post_start_script_url': psscript,
                 'image_id': image_id
         }
         self.bcc._make_post_request = MagicMock(return_value=simplejson.dumps("Success"))
 
-        result = self.bcc.create_cluster(cluster_name, password, cloud_type, access_key, secret_key, instance_type, placement=zone, post_start_script=psscript, worker_script=wscript, ami=image_id)
-        
+        user_data = {
+                'access_key': access_key,
+                'secret_key': secret_key,
+                'cluster_name': cluster_name,
+                'cloud_type': cloud_name,
+                'freenxpass':'',
+                'password': password,
+                's3_port': '',
+                'region_name': zone,
+                'cidr_range': '',
+                'region_endpoint': '',
+                'ec2_port': '',
+                'bucket_default': '',
+                's3_host': '',
+                'is_secure': 'False',
+                's3_conn_path': '/',
+                'ec2_conn_path': '/services/Cloud',
+                'image_id': image_id
+        }
+        result = self.bcc.launch(instance_type, ud=user_data, cloud_name=cloud_name)
+
+        #result = self.bcc.create_cluster(cluster_name, password, cloud_type, access_key, secret_key, instance_type, placement=zone, post_start_script=psscript, worker_script=wscript, ami=image_id)
+
         self.bcc._make_post_request.assert_called_with("/api/launch", parameters=launch_hash)
 
 
