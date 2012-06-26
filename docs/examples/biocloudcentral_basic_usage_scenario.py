81215578200554ec8695dc0386976ce5a21df6fa
Gregory Long <gregorydavidlong@gmail.com>
2012-06-26 18:29:38 +1000

    Documented/tweaked BioCloudCentral example usage scenario.

--- docs/examples/biocloudcentral_basic_usage_scenario.py
+++ docs/examples/biocloudcentral_basic_usage_scenario.py
@@ -2,34 +2,40 @@ import re
 import simplejson
 from blend import bcc
 
-
+# URL of BioCloudCentral
 url = "http://127.0.0.1:8000"
+
+# Ec2 credentials
 access_key = 'access_key'
 secret_key = 'secret_key'
+
+# Create a BioCloudCentral connection
 bcc = bcc.BCC(url)
 
+# Get the supported clouds
 clouds = bcc.get_clouds()
 cloud_name = clouds[1]['name']
 cloud_type = clouds[1]['cloud_type']
 
+# Get the supported machine images
 images = bcc.get_images()
 image_id = images[2]['id']
 
+# Get the supported instance types
 instance_types = bcc.get_instance_types()
 instance_type = instance_types[-1]['tech_name']
 
+# Get the supported regions for the cloud and instance type
 regions = bcc.get_regions(cloud_name, access_key, secret_key, instance_type)
 region = regions[0]
 
 cluster_name = "My cluster"
 password = 'password'
 
-psscript = ''
-wscript = ''
-
+# Create the user data object
 user_data = {
-        'cluster_name': 'Test Cluster',
-        'password': 'password',
+        'cluster_name': cluster_name,
+        'password': password,
         'cloud_type': cloud_type,
         'access_key': access_key,
         'secret_key': secret_key,
@@ -37,5 +43,6 @@ user_data = {
         'image_id': image_id
         }
 
+# Launch an instance
 result = bcc.launch(instance_type, ud=user_data, cloud_name=cloud_name)
 print result
