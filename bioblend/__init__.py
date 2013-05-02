be2a540a3c15d93a13b27fa5c0867f34410465ec
Enis Afgan <afgane@gmail.com>
2013-05-02 16:01:44 +0200

    Bump version

--- bioblend/__init__.py
+++ bioblend/__init__.py
@@ -3,7 +3,7 @@ import logging
 from bioblend.config import Config, BioBlendConfigLocations
 
 # Current version of the library
-__version__ = '0.2.3-dev'
+__version__ = '0.2.4-dev'
 
 config = Config()
 
