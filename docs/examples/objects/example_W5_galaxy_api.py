41423008d1f8a44b1c706b7ec8db9bd38f038fc3
simleo <simleo@crs4.it>
2014-03-04 12:22:57 +0100

    updated W5_galaxy_api example

--- docs/examples/objects/example_W5_galaxy_api.py
+++ docs/examples/objects/example_W5_galaxy_api.py
@@ -1,82 +1,86 @@
-# This example will use a workflow and datasets publicly available on
-# Orione Galaxy server.
-URL = 'http://orione.crs4.it/'
-# To use the Galaxy API you need an API key. Procedure:
-# 1) go to http://orione.crs4.it/ and register, or login if you are
-#    already registered, through the "User" menu on the top;
-# 2) open "User" -> "API Keys";
-# 3) generate your API key if you don't already have one;
-# 4) replace YOUR_API_KEY with your API key on the line below.
+import sys, os, json
+
+# This example uses a workflow and datasets publicly available on
+# CRS4's Orione Galaxy server.
+
+URL = 'http://orione.crs4.it'
+
+# To use the Galaxy API you need an API key. To get one, proceed as follows:
+#   1) go to http://orione.crs4.it and register, or log in if you are
+#      already registered, through the "User" menu at the top of the page;
+#   2) open "User" -> "API Keys";
+#   3) generate your API key if you don't have one;
+#   4) in the following code, replace YOUR_API_KEY with your API key.
+
 API_KEY = 'YOUR_API_KEY'
-# Make a local clone of https://bitbucket.org/galaxy/galaxy-dist/ and
-# replace YOUR_GALAXY_PATH with its path on the line below
+
+# Clone the galaxy-dist mercurial repository and replace
+# YOUR_GALAXY_PATH with the clone's local path in the following code, e.g.:
+#   cd /tmp
+#   hg clone https://bitbucket.org/galaxy/galaxy-dist
+#   GALAXY_PATH = '/tmp/galaxy-dist'
+
 GALAXY_PATH = 'YOUR_GALAXY_PATH'
-
-import json
-import os.path
-import sys
-import urlparse
-
 sys.path.insert(1, os.path.join(GALAXY_PATH, 'scripts/api'))
 import common
 
-# Get all published workflows and select a published workflow by name
-workflows = common.get(API_KEY, urlparse.urljoin(URL, 'api/workflows?show_published=True'))
-published_workflows = [workflow for workflow in workflows if workflow['published']]
+# Select "W5 - Metagenomics" from published workflows
+
 workflow_name = 'W5 - Metagenomics'
-w = [pw for pw in published_workflows if pw['name'] == workflow_name]
-if len(w) != 1:
-    raise Exception, 'Wrong number of workflows found for name "%s"' % workflow_name
+workflows = common.get(API_KEY, '%s/api/workflows?show_published=True' % URL)
+w = [_ for _ in workflows if _['published'] and _['name'] == workflow_name]
+assert len(w) == 1
 w = w[0]
 
-# Import the selected published workflow
-# This is not needed if you are the owner of the workflow, an admin or
-# if the workflow is shared with you. It would probably make sense to
-# change the API to let a user run directly importable workflows too
-data = {}
-data['workflow_id'] = w['id']
-iw = common.post(API_KEY, urlparse.urljoin(URL, 'api/workflows/import'), data)
-iw_details = common.get(API_KEY, URL + 'api/workflows/' + iw['id'])
+# Import the workflow to user space
 
-# Get all data libraries and select a library by name
-libraries = common.get(API_KEY, urlparse.urljoin(URL, 'api/libraries'))
+data = {'workflow_id': w['id']}
+iw = common.post(API_KEY, '%s/api/workflows/import' % URL, data)
+iw_details = common.get(API_KEY, '%s/api/workflows/%s' % (URL, iw['id']))
+
+# Select the "Orione SupMat" library
+
 library_name = 'Orione SupMat'
-l = [library for library in libraries if library['name'] == library_name]
-if len(l) != 1:
-    raise Exception, 'Wrong number of libraries found for name "%s"' % library_name
+libraries = common.get(API_KEY, '%s/api/libraries' % URL)
+l = [_ for _ in libraries if _['name'] == library_name]
+assert len(l) == 1
 l = l[0]
 
-# Get the contents of the selected library and select a dataset by name
-l_contents = common.get(API_KEY, urlparse.urljoin(URL, 'api/libraries/' + l['id'] + '/contents'))
-library_dataset_name = '/Metagenomics/MetagenomicsDataset.fq'
-ld = [l_content for l_content in l_contents if l_content['type'] == 'file' and l_content['name'] == library_dataset_name]
-if len(ld) != 1:
-    raise Exception, 'Wrong number of library datasets found for name "%s"' % library_dataset_name
-ld = ld[0]
-ld_details = common.get(API_KEY, urlparse.urljoin(URL, 'api/libraries/' + l['id'] + '/contents/' + ld['id']))
-ld_ldda_id = ld_details['ldda_id']
+# Select the "/Metagenomics/MetagenomicsDataset.fq" dataset
 
-# Select a workflow step by tool id
+ds_name = '/Metagenomics/MetagenomicsDataset.fq'
+contents = common.get(API_KEY, '%s/api/libraries/%s/contents' % (URL, l['id']))
+ld = [_ for _ in contents if _['type'] == 'file' and _['name'] == ds_name]
+assert len(ld) == 1
+ld = ld[0]
+
+# Select the "ncbi_blastn_wrapper" step
+
 tool_id = 'ncbi_blastn_wrapper'
-ws = [step for step in iw_details['steps'].itervalues() if step['tool_id'] == tool_id]
-if len(ws) != 1:
-    raise Exception, 'Wrong number of workflow steps found for tool id "%s"' % tool_id
+ws = [_ for _ in iw_details['steps'].itervalues() if _['tool_id'] == tool_id]
+assert len(ws) == 1
 ws = ws[0]
 
-# Get (a copy of) the tool parameters for the selected workflow step
+# Get (a copy of) the parameters dict for the selected step
+
 ws_parameters = ws['tool_inputs'].copy()
 for k, v in ws_parameters.iteritems():
     ws_parameters[k] = json.loads(v)
 
-# Run the imported workflow on a new history with the selected dataset
-# as input and modifying a tool parameter for the selected workflow step
-history_name = workflow_name
-data = {}
-data['workflow_id'] = iw['id']
-data['parameters'] = {tool_id: {'db_opts': ws_parameters['db_opts']}}
-data['parameters'][tool_id]['db_opts']['database'] = '16SMicrobial-20131106'
-iw_input_step_ids = iw_details['inputs'].keys()
-data['ds_map'] = { iw_input_step_ids[0]: dict(src='ld', id=ld['id']) }
-data['history'] = history_name
-r_dict = common.post(API_KEY, urlparse.urljoin(URL, 'api/workflows'), data)
+# Run the workflow on a new history with the selected dataset
+# as input, setting the BLAST db to "16SMicrobial-20131106"
 
+history_name = workflow_name
+ws_parameters['db_opts']['database'] = '16SMicrobial-20131106'
+data = {
+    'workflow_id': iw['id'],
+    'parameters': {tool_id: {'db_opts': ws_parameters['db_opts']}},
+    }
+assert len(iw_details['inputs']) == 1
+input_step_id = iw_details['inputs'].keys()[0]
+data['ds_map'] = {input_step_id: {'src': 'ld', 'id' : ld['id']}}
+data['history'] = history_name
+r_dict = common.post(API_KEY, '%s/api/workflows' % URL, data)
+
+print "Running workflow: %s [%s]" % (iw['name'], iw['id'])
+print "Output history: %s [%s]" % (history_name, r_dict['history'])
