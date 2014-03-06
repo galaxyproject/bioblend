f53c0eb18519f91df7dd82fc87f3464561902738
simleo <simleo@crs4.it>
2014-03-06 18:26:43 +0100

    updated W5_bioblend.objects example

--- docs/examples/objects/example_W5_bioblend.objects.py
+++ docs/examples/objects/example_W5_bioblend.objects.py
@@ -1,53 +1,61 @@
-# This example will use a workflow and datasets publicly available on
-# Orione Galaxy server.
-URL = 'http://orione.crs4.it/'
-# To use the Galaxy API you need an API key. Procedure:
-# 1) go to http://orione.crs4.it/ and register, or login if you are
-#    already registered, through the "User" menu on the top;
-# 2) open "User" -> "API Keys";
-# 3) generate your API key if you don't already have one;
-# 4) replace YOUR_API_KEY with your API key on the line below.
-API_KEY = 'YOUR_API_KEY'
-
 from bioblend.galaxy.objects import *
 
+# This example uses a workflow and datasets publicly available on
+# CRS4's Orione Galaxy server.
+
+URL = 'http://orione.crs4.it'
+
+# To use the Galaxy API you need an API key. To get one, proceed as follows:
+#   1) go to http://orione.crs4.it and register, or log in if you are
+#      already registered, through the "User" menu on the top;
+#   2) open "User" -> "API Keys";
+#   3) generate your API key if you don't have one;
+#   4) in the following code, replace YOUR_API_KEY with your API key.
+
+API_KEY = 'YOUR_API_KEY'
 gi = GalaxyInstance(URL, API_KEY)
 
-# Select workflows by name and restrict to published workflows
+# Select "W5 - Metagenomics" from published workflows
+
 workflow_name = 'W5 - Metagenomics'
-workflows = gi.workflows.list(name=workflow_name, published=True)
-w = [workflow for workflow in workflows if workflow.published]
-if len(w) != 1:
-    raise Exception, 'Wrong number of published workflows found for name "%s"' % workflow_name
-w = w[0]
+previews = gi.workflows.get_previews(name=workflow_name, published=True)
+p = [_ for _ in previews if _.published]
+assert len(p) == 1
+p = p[0]
 
-# Import the selected published workflow
-iw = gi.workflows.import_shared(w.id)
+# Import the workflow to user space
 
-# Select a data library by name
+iw = gi.workflows.import_shared(p.id)
+
+# Select the "Orione SupMat" library
+
 library_name = 'Orione SupMat'
-l = gi.libraries.list(library_name)
-if len(l) != 1:
-    raise Exception, 'Wrong number of libraries found for name "%s"' % library_name
+l = gi.libraries.list(name=library_name)
+assert len(l) == 1
 l = l[0]
 
-# Get the contents of the selected library and select a dataset by name
-#TODO
+# Select the "/Metagenomics/MetagenomicsDataset.fq" dataset
 
-# Select a workflow step by tool id
+ds_name = '/Metagenomics/MetagenomicsDataset.fq'
+ld = l.get_datasets(name=ds_name)
+assert len(ld) == 1
+ld = ld[0]
+
+# Select the "ncbi_blastn_wrapper" step
+
 tool_id = 'ncbi_blastn_wrapper'
-ws = [step for step in iw.tools if step.tool_id == tool_id]
-if len(ws) != 1:
-    raise Exception, 'Wrong number of workflow steps found for tool id "%s"' % tool_id
+ws = [_ for _ in iw.info.steps.itervalues() if _['tool_id'] == tool_id]
+assert len(ws) == 1
 ws = ws[0]
 
-# Get (a copy of) the tool parameters for the selected workflow step
-ws_parameters = ws.tool_state.copy()
+# Get (a copy of) the parameters dict for the selected step
 
-# Run the imported workflow on a new history with the selected dataset
-# as input and modifying a tool parameter for the selected workflow step
+ws_parameters = ws['tool_inputs'].copy()
+
+# Run the workflow on a new history with the selected dataset
+# as input, setting the BLAST db to "16SMicrobial-20131106"
+
 history_name = workflow_name
 params = {tool_id: {'db_opts': ws_parameters['db_opts']}}
 params[tool_id]['db_opts']['database'] = '16SMicrobial-20131106'
-inputs = [] #TODO
-iw.run(inputs, history_name, params=params)
+iw.run([ld], history_name, params=params)
