258524f44a6977e8f78471c76d5a5d339538aa07
John Chilton <jmchilton@gmail.com>
2014-03-08 00:44:19 -0600

    Galaxy tool client bug fix and enhancements.
    Running tools besides upload1 didn't work because files weren't attached and so mimetype handling was off. See comments in changeset for details. This commit adds a helper method to the tools client to handle this transparently for layers above it - update upload_file and run_tool to use this.
    
    Add some very rudimentary tests for the tools client.

--- bioblend/galaxy/tools/__init__.py
+++ bioblend/galaxy/tools/__init__.py
@@ -21,8 +21,8 @@ class ToolClient(Client):
         payload = {}
         payload["history_id"] = history_id
         payload["tool_id"] = tool_id
-        payload["inputs"] = dumps(tool_inputs)
-        return Client._post(self, payload)
+        payload["inputs"] = tool_inputs
+        return self._tool_post(payload)
 
     def upload_file(self, path, history_id, **keywords):
         """
@@ -32,13 +32,28 @@ class ToolClient(Client):
         payload = {}
         payload["history_id"] = history_id
         payload["tool_id"] = keywords.get("tool_id", "upload1")
+        default_file_name = basename(path)
         file_type = keywords.get("file_type", "auto")
-        file_name = keywords.get("file_name", basename(path))
+        file_name = keywords.get("file_name", default_file_name)
         tool_input = {}
         tool_input["file_type"] = file_type
         tool_input["dbkey"] = keywords.get("dbkey", "?")
         tool_input["files_0|NAME"] = file_name
         tool_input["files_0|type"] = "upload_dataset"
-        payload["inputs"] = dumps(tool_input)
-        payload["files_0|file_data"] = open(path, 'rb')
-        return Client._post(self, payload, files_attached=True)
+        payload["inputs"] = tool_input
+        payload["files_0|file_data"] = open(path, "rb")
+        return self._tool_post(payload, files_attached=True)
+
+    def _tool_post(self, payload, files_attached=False):
+        if files_attached:
+            # If files_attached - this will be posted as multi-part form data
+            # and so each individual parameter needs to be encoded so can be
+            # decoded as JSON by Galaxy (hence dumping complex parameters).
+            # If not files are attached the whole thing is posted a
+            # application/json and dumped/loaded all at once by requests and
+            # Galaxy.
+            complex_payload_params = ["inputs"]
+            for key in complex_payload_params:
+                if key in payload:
+                    payload[key] = dumps(payload[key])
+        return Client._post(self, payload, files_attached=files_attached)
