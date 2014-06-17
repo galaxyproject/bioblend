185d98faeb7e69acc8edca607e3ad4a53a7f8840
Nicola Soranzo <soranzo@crs4.it>
2014-06-17 17:53:13 +0200

    Deprecate create_user() method in favor of clearer create_remote_user(). Add create_local_user().
    create_local_user() is also going to be used for Continuous Integration testing.

--- tests/TestGalaxyUsers.py
+++ tests/TestGalaxyUsers.py
@@ -26,9 +26,18 @@ class TestGalaxyUsers(GalaxyTestBase.GalaxyTestBase):
         self.assertEqual(user['nice_total_disk_usage'], current_user['nice_total_disk_usage'])
         self.assertEqual(user['total_disk_usage'], current_user['total_disk_usage'])
 
-    def test_create_user(self):
-        # TODO: Need to have permissions to create user!
-        # users = self.gi.users.create_user('buildbot@galaxy-vic.genome.edu.au')
+    def test_create_remote_user(self):
+        # WARNING: only admins can create users!
+        # users = self.gi.users.create_remote_user('newuser@example.com')
+        # TODO: Since users cannot be deleted, this test cannot be executed
+        #       on a normal Galaxy instance, but may be activated for TravisCI.
+        pass
+
+    def test_create_local_user(self):
+        # WARNING: only admins can create users!
+        # users = self.gi.users.create_local_user('newuser', 'newuser@example.com', 'secret')
+        # TODO: Since users cannot be deleted, this test cannot be executed
+        #       on a normal Galaxy instance, but may be activated for TravisCI.
         pass
 
     def test_get_current_user(self):
