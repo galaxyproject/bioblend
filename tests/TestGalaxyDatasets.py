6e56d3fa8a43dfb5f7cbb600f82717e1fe8b385f
abretaud <anthony.bretaudeau@rennes.inra.fr>
2014-12-18 17:12:01 +0100

    add some tests

--- tests/TestGalaxyDatasets.py
+++ tests/TestGalaxyDatasets.py
@@ -15,3 +15,11 @@ class TestGalaxyDatasets(GalaxyTestBase.GalaxyTestBase):
     def test_download_dataset(self):
         with self.assertRaises(Exception):
             dataset = self.gi.datasets.download_dataset(None)
+
+    def test_show_stderr(self):
+        stderr = self.gi.datasets.show_stderr()
+        self.assertIsNotNone(stderr)
+
+    def test_show_stdout(self):
+        stdout = self.gi.datasets.show_stdout()
+        self.assertIsNotNone(stdout)
