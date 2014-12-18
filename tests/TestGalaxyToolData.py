ef556931402a6fe70c1a1a6f6252749b4b9aa185
abretaud <anthony.bretaudeau@rennes.inra.fr>
2014-12-18 17:12:01 +0100

    Add some tests for roles and tool data.

---
+++ tests/TestGalaxyToolData.py
@@ -0,0 +1,24 @@
+"""
+Tests the functionality of the Blend CloudMan API. These tests require working
+credentials to supported cloud infrastructure.
+
+Use ``nose`` to run these unit tests.
+"""
+import GalaxyTestBase
+import test_util
+
+
+@test_util.skip_unless_galaxy()
+class TestGalaxyToolData(GalaxyTestBase.GalaxyTestBase):
+
+    def test_get_data_tables(self):
+        tables = self.gi.tool_data.get_data_tables()
+        for table in tables:
+            self.assertIsNotNone(table['name'])
+
+    def test_show_data_table(self):
+        tables = self.gi.tool_data.get_data_tables()
+        table = self.gi.tool_data.show_data_table(tables[0]['name'])
+        self.assertIsNotNone(table['columns'])
+        self.assertIsNotNone(table['fields'])
+        self.assertIsNotNone(table['name'])
