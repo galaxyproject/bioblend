bfcec907f0065d595c13a5e63194e7ab4684973c
Enis Afgan <afgane@gmail.com>
2015-06-30 12:20:40 -0400

    Update version for the 0.6.0 release

--- bioblend/__init__.py
+++ bioblend/__init__.py
@@ -4,7 +4,7 @@ import os
 from bioblend.config import Config, BioBlendConfigLocations
 
 # Current version of the library
-__version__ = '0.5.4-dev'
+__version__ = '0.6.0'
 
 # default chunk size (in bytes) for reading remote data
 try:
