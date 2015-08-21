b868d9fccf807b05637f1b7a10916a4a06e66a97
Enis Afgan <afgane@gmail.com>
2015-08-21 13:01:21 -0400

    Bump dev version to 0.7.0 due to the method signature changes from the previous two commits

--- bioblend/__init__.py
+++ bioblend/__init__.py
@@ -4,7 +4,7 @@ import os
 from bioblend.config import Config, BioBlendConfigLocations
 
 # Current version of the library
-__version__ = '0.6.2-dev'
+__version__ = '0.7.0-dev'
 
 # default chunk size (in bytes) for reading remote data
 try:
