galaxyproject/bioblend

commit 8705aa1e294d758f7c3e25970dc082bb791df727

Add extra parameters to histories.update_history() and History.update() .
Add test_update_history to TestGalaxyHistories.py .
Fix some typos.

bioblend/galaxy/histories/__init__.py
@@ -81,7 +81,7 @@ class HistoryClient(Client):
 
     def delete_dataset_collection(self, history_id, dataset_collection_id):
         """
-        Mark corresponding datset as deleted.
+        Mark corresponding dataset as deleted.
         """
         url = self.gi._make_url(self, history_id, contents=True)
         # Append the dataset_id to the base history contents URL
@@ -133,34 +133,37 @@ class HistoryClient(Client):
         url = '/'.join([url, dataset_id, "provenance"])
         return Client._get(self, url=url)
 
-    def update_history(self, history_id, name=None, annotation=None):
+    def update_history(self, history_id, name=None, annotation=None, **kwds):
         """
-        Update history metadata information. Current attributes that can be modified
-        for a history 'name' and 'annotation'.
+        Update history metadata information. Some of the attributes that can be
+        modified are documented below.
 
         :type history_id: string
         :param history_id: Encoded history ID
         :type name: string
-        :param name: Replace history with the given string
+        :param name: Replace history name with the given string
         :type annotation: string
         :param annotation: Replace history annotation with given string
+        :type deleted: boolean
+        :param deleted: Mark or unmark history as deleted
+        :type published: boolean
+        :param published: Mark or unmark history as published
+        :type importable: boolean
+        :param importable: Mark or unmark history as importable
+        :type tags: list
+        :param tags: Replace history tags with the given list
 
         :rtype: status_code (int)
 
         """
-
-        payload = {}
-        if name:
-            payload['name'] = name
-        if annotation:
-            payload['annotation'] = annotation
-
-        return Client._put(self, payload, id=history_id).status_code
+        kwds['name'] = name
+        kwds['annotation'] = annotation
+        return Client._put(self, kwds, id=history_id).status_code
 
     def update_dataset(self, history_id, dataset_id, **kwds):
         """
-        Update history dataset metadata. Only a subset of the available
-        metadata parameters for modification are documented below currently.
+        Update history dataset metadata. Some of the attributes that can be
+        modified are documented below.
 
         :type history_id: string
         :param history_id: Encoded history ID
@@ -169,9 +172,9 @@ class HistoryClient(Client):
         :type annotation: string
         :param annotation: Replace history dataset annotation with given string
         :type deleted: boolean
-        :param deleted: Mark or unmark history dataset as deleted.
+        :param deleted: Mark or unmark history dataset as deleted
         :type visible: boolean
-        :param visible: Mark or unmark history dataset as visible.
+        :param visible: Mark or unmark history dataset as visible
 
         :rtype: status_code (int)
         """
@@ -182,8 +185,8 @@ class HistoryClient(Client):
 
     def update_dataset_collection(self, history_id, dataset_collection_id, **kwds):
         """
-        Update history dataset metadata. Only a subset of the available
-        metadata parameters for modification are documented below currently.
+        Update history dataset collection metadata. Some of the attributes that
+        can be modified are documented below.
 
         :type history_id: string
         :param history_id: Encoded history ID
@@ -266,7 +269,7 @@ class HistoryClient(Client):
         elif d_type is None and 'data_type' in meta:
             d_type = meta['data_type']
 
-        # TODO: Download this via the RET API. api/datasets/<dataset_id>/display
+        # TODO: Download this via the REST API. api/datasets/<dataset_id>/display
         download_url = 'datasets/' + meta['id'] + '/display?to_ext=' + d_type
         url = urlparse.urljoin(self.gi.base_url, download_url)
 

bioblend/galaxy/objects/wrappers.py
@@ -757,15 +757,29 @@ class History(DatasetContainer):
     def gi_module(self):
         return self.gi.histories
 
-    def update(self, name=None, annotation=None):
+    def update(self, name=None, annotation=None, **kwds):
         """
-        Update history metadata with the given name and annotation.
+        Update history metadata information. Some of the attributes that can be
+        modified are documented below.
+
+        :type name: string
+        :param name: Replace history name with the given string
+        :type annotation: string
+        :param annotation: Replace history annotation with given string
+        :type deleted: boolean
+        :param deleted: Mark or unmark history as deleted
+        :type published: boolean
+        :param published: Mark or unmark history as published
+        :type importable: boolean
+        :param importable: Mark or unmark history as importable
+        :type tags: list
+        :param tags: Replace history tags with the given list
         """
         # TODO: wouldn't it be better if name and annotation were attributes?
         # TODO: do we need to ensure the attributes of `self` are the same as
         # the ones returned by the call to `update_history` below?
         res = self.gi.gi.histories.update_history(
-            self.id, name=name, annotation=annotation
+            self.id, name=name, annotation=annotation, **kwds
             )
         if res != httplib.OK:
             raise RuntimeError('failed to update history')

tests/TestGalaxyHistories.py
@@ -22,6 +22,17 @@ class TestGalaxyHistories(GalaxyTestBase.GalaxyTestBase):
         self.assertEqual(new_history['name'], history_name)
         self.assertIsNotNone(new_history['url'])
 
+    def test_update_history(self):
+        new_name = 'buildbot - automated test renamed'
+        new_annotation = 'Annotation for %s' % new_name
+        new_tags = ['tag1', 'tag2']
+        self.gi.histories.update_history(self.history['id'], name=new_name, annotation=new_annotation, tags=new_tags)
+        updated_hist = self.gi.histories.show_history(self.history['id'])
+        self.assertEqual(self.history['id'], updated_hist['id'])
+        self.assertEqual(updated_hist['name'], new_name)
+        self.assertEqual(updated_hist['annotation'], new_annotation)
+        self.assertEqual(updated_hist['tags'], new_tags)
+
     def test_get_histories(self):
         # Make sure there's at least one value - the one we created
         full_history = self.gi.histories.get_histories()

tests/TestGalaxyObjects.py
@@ -559,10 +559,12 @@ class TestHistory(GalaxyObjectsTestBase):
     def test_update(self):
         new_name = 'test_%s' % uuid.uuid4().hex
         new_annotation = 'Annotation for %s' % new_name
-        updated_hist = self.hist.update(name=new_name, annotation=new_annotation)
+        new_tags = ['tag1', 'tag2']
+        updated_hist = self.hist.update(name=new_name, annotation=new_annotation, tags=new_tags)
         self.assertEqual(self.hist.id, updated_hist.id)
         self.assertEqual(self.hist.name, new_name)
         self.assertEqual(self.hist.annotation, new_annotation)
+        self.assertEqual(self.hist.tags, new_tags)
 
 
 @test_util.skip_unless_galaxy()

commit b8a9ce6cda06c04ce5bb0a3b138389372fa5a59f

Fix HistoryClient.update_history() and History.update() methods when 'name' is not specified
Fix https://github.com/galaxyproject/bioblend/issues/214 .

Broken in commit 8705aa1e294d758f7c3e25970dc082bb791df727 .

BioBlend.objects: add 'published' attribute to 'History' objects.

bioblend/_tests/TestGalaxyHistories.py
@@ -34,6 +34,11 @@ class TestGalaxyHistories(GalaxyTestBase.GalaxyTestBase):
         self.assertEqual(updated_hist['name'], new_name)
         self.assertEqual(updated_hist['annotation'], new_annotation)
         self.assertEqual(updated_hist['tags'], new_tags)
+        updated_hist = self.gi.histories.update_history(self.history['id'], published=True)
+        if 'id' not in updated_hist:
+            updated_hist = self.gi.histories.show_history(self.history['id'])
+        self.assertEqual(self.history['id'], updated_hist['id'])
+        self.assertTrue(updated_hist['published'])
 
     def test_get_histories(self):
         # Make sure there's at least one value - the one we created

bioblend/_tests/TestGalaxyObjects.py
@@ -620,6 +620,9 @@ class TestHistory(GalaxyObjectsTestBase):
         self.assertEqual(self.hist.name, new_name)
         self.assertEqual(self.hist.annotation, new_annotation)
         self.assertEqual(self.hist.tags, new_tags)
+        updated_hist = self.hist.update(published=True)
+        self.assertEqual(self.hist.id, updated_hist.id)
+        self.assertTrue(self.hist.published)
 
     def test_create_dataset_collection(self):
         self._create_collection_description()

bioblend/galaxy/histories/__init__.py
@@ -221,7 +221,7 @@ class HistoryClient(Client):
         url = '/'.join([url, dataset_id, "provenance"])
         return self._get(url=url)
 
-    def update_history(self, history_id, name=None, annotation=None, **kwds):
+    def update_history(self, history_id, **kwds):
         """
         Update history metadata information. Some of the attributes that can be
         modified are documented below.
@@ -237,6 +237,10 @@ class HistoryClient(Client):
 
         :type deleted: bool
         :param deleted: Mark or unmark history as deleted
+
+        :type purged: bool
+        :param purged: If True, mark history as purged (permanently deleted).
+            Ignored on Galaxy release_15.01 and earlier
 
         :type published: bool
         :param published: Mark or unmark history as published
@@ -255,8 +259,6 @@ class HistoryClient(Client):
             The return value was changed in BioBlend v0.8.0, previously it was
             the status code (type int).
         """
-        kwds['name'] = name
-        kwds['annotation'] = annotation
         return self._put(payload=kwds, id=history_id)
 
     def update_dataset(self, history_id, dataset_id, **kwds):

bioblend/galaxy/objects/wrappers.py
@@ -902,7 +902,7 @@ class History(DatasetContainer):
     """
     Maps to a Galaxy history.
     """
-    BASE_ATTRS = DatasetContainer.BASE_ATTRS + ('annotation', 'state', 'state_ids', 'state_details', 'tags')
+    BASE_ATTRS = DatasetContainer.BASE_ATTRS + ('annotation', 'published', 'state', 'state_ids', 'state_details', 'tags')
     DS_TYPE = HistoryDatasetAssociation
     DSC_TYPE = HistoryDatasetCollectionAssociation
     CONTENT_INFO_TYPE = HistoryContentInfo
@@ -916,7 +916,7 @@ class History(DatasetContainer):
     def gi_module(self):
         return self.gi.histories
 
-    def update(self, name=None, annotation=None, **kwds):
+    def update(self, **kwds):
         """
         Update history metadata information. Some of the attributes that can be
         modified are documented below.
@@ -930,6 +930,10 @@ class History(DatasetContainer):
         :type deleted: bool
         :param deleted: Mark or unmark history as deleted
 
+        :type purged: bool
+        :param purged: If True, mark history as purged (permanently deleted).
+            Ignored on Galaxy release_15.01 and earlier
+
         :type published: bool
         :param published: Mark or unmark history as published
 
@@ -940,10 +944,7 @@ class History(DatasetContainer):
         :param tags: Replace history tags with the given list
         """
         # TODO: wouldn't it be better if name and annotation were attributes?
-        # TODO: do we need to ensure the attributes of `self` are the same as
-        # the ones returned by the call to `update_history` below?
-        self.gi.gi.histories.update_history(self.id, name=name,
-                                            annotation=annotation, **kwds)
+        self.gi.gi.histories.update_history(self.id, **kwds)
         self.refresh()
         return self
 