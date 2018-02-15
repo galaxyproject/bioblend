972893fce557e90958d9adcbbfde3d018c845742
dpryan79 <dpryan79@gmail.com>
2018-02-15 13:51:03 +0100

    Add a verify option when using the object-oriented API to match the normal API

--- bioblend/galaxy/objects/galaxy_instance.py
+++ bioblend/galaxy/objects/galaxy_instance.py
@@ -47,8 +47,8 @@ class GalaxyInstance(object):
       gi = GalaxyInstance('http://127.0.0.1:8080', 'foo')
       histories = gi.histories.list()
     """
-    def __init__(self, url, api_key=None, email=None, password=None):
-        self.gi = bioblend.galaxy.GalaxyInstance(url, api_key, email, password)
+    def __init__(self, url, api_key=None, email=None, password=None, verify=True):
+        self.gi = bioblend.galaxy.GalaxyInstance(url, api_key, email, password, verify)
         self.log = bioblend.log
         self.histories = client.ObjHistoryClient(self)
         self.libraries = client.ObjLibraryClient(self)
