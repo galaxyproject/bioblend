20ee8fc8ea2a9fbe2b290b4977ee746250eb48b6
Simon Bray <sbray1371@gmail.com>
2019-11-20 17:01:44 +0100

    combine all invocation tests into 1 function

--- bioblend/_tests/TestGalaxyInvocations.py
+++ bioblend/_tests/TestGalaxyInvocations.py
@@ -8,54 +8,6 @@ from . import GalaxyTestBase, test_util
 
 class TestGalaxyInvocations(GalaxyTestBase.GalaxyTestBase):
     @test_util.skip_unless_galaxy('release_19.09')
-    def test_workflow_scheduling(self):
-        path = test_util.get_abspath(os.path.join('data', 'test_workflow_pause.ga'))
-        workflow = self.gi.workflows.import_workflow_from_local_path(path)
-        workflow_id = workflow["id"]
-        history_id = self.gi.histories.create_history(name="TestWorkflowState")["id"]
-        dataset1_id = self._test_dataset(history_id)
-
-        invocations = self.gi.workflows.get_invocations(workflow_id)
-        self.assertEqual(len(invocations), 0)
-
-        invocation = self.gi.workflows.invoke_workflow(
-            workflow["id"],
-            inputs={"0": {"src": "hda", "id": dataset1_id}},
-        )
-        invocation_id = invocation["id"]
-        invocations = self.gi.workflows.get_invocations(workflow_id)
-        self.assertEqual(len(invocations), 1)
-        self.assertEqual(invocations[0]["id"], invocation_id)
-
-        def invocation_steps_by_order_index():
-            invocation = self.gi.workflows.show_invocation(workflow_id, invocation_id)
-            return dict((s["order_index"], s) for s in invocation["steps"])
-
-        for i in range(20):
-            if 2 in invocation_steps_by_order_index():
-                break
-            time.sleep(.5)
-
-        invocation = self.gi.workflows.show_invocation(workflow_id, invocation_id)
-        self.assertEqual(invocation['state'], "ready")
-
-        steps = invocation_steps_by_order_index()
-        pause_step = steps[2]
-        self.assertIsNone(
-            self.gi.workflows.show_invocation_step(workflow_id, invocation_id, pause_step["id"])["action"])
-        self.gi.workflows.run_invocation_step_action(workflow_id, invocation_id, pause_step["id"], action=True)
-        self.assertTrue(self.gi.workflows.show_invocation_step(workflow_id, invocation_id, pause_step["id"])["action"])
-        for i in range(20):
-            invocation = self.gi.workflows.show_invocation(workflow_id, invocation_id)
-            if invocation["state"] == "scheduled":
-                break
-
-            time.sleep(.5)
-
-        invocation = self.gi.workflows.show_invocation(workflow_id, invocation_id)
-        self.assertEqual(invocation["state"], "scheduled")
-
-    @test_util.skip_unless_galaxy('release_19.09')
     @test_util.skip_unless_tool("cat1")
     @test_util.skip_unless_tool("cat")
     def test_cancelling_workflow_scheduling(self):
@@ -65,7 +17,7 @@ class TestGalaxyInvocations(GalaxyTestBase.GalaxyTestBase):
         history_id = self.gi.histories.create_history(name="TestWorkflowState")["id"]
         dataset1_id = self._test_dataset(history_id)
 
-        invocations = self.gi.workflows.get_invocations(workflow_id)
+        invocations = self.gi.invocations.get_invocations()
         self.assertEqual(len(invocations), 0)
 
         invocation = self.gi.workflows.invoke_workflow(
@@ -73,32 +25,26 @@ class TestGalaxyInvocations(GalaxyTestBase.GalaxyTestBase):
             inputs={"0": {"src": "hda", "id": dataset1_id}},
         )
         invocation_id = invocation["id"]
-        invocations = self.gi.workflows.get_invocations(workflow_id)
+        invocations = self.gi.invocations.get_invocations()
         self.assertEqual(len(invocations), 1)
         self.assertEqual(invocations[0]["id"], invocation_id)
 
-        invocation = self.gi.workflows.show_invocation(workflow_id, invocation_id)
+        invocation = self.gi.invocations.show_invocation(invocation_id)
         self.assertIn(invocation['state'], ['new', 'ready'])
 
-        self.gi.workflows.cancel_invocation(workflow_id, invocation_id)
-        invocation = self.gi.workflows.show_invocation(workflow_id, invocation_id)
+        self.gi.invocations.cancel_invocation(invocation_id)
+        invocation = self.gi.invocations.show_invocation(invocation_id)
         self.assertEqual(invocation['state'], 'cancelled')
 
-    @test_util.skip_unless_galaxy('release_19.09')
-    def test_invoke_workflow(self):
-        path = test_util.get_abspath(os.path.join('data', 'paste_columns.ga'))
-        wf = self.gi.workflows.import_workflow_from_local_path(path)
-        history_id = self.gi.histories.create_history(name="test_wf_invocation")['id']
-        dataset1_id = self._test_dataset(history_id)
-        dataset = {'src': 'hda', 'id': dataset1_id}
-        invoke_response = self.gi.workflows.invoke_workflow(
-            wf['id'],
-            inputs={'Input 1': dataset, 'Input 2': dataset},
-            history_id=history_id,
-            inputs_by='name',
-        )
-        summary = self.gi.workflows.get_invocation_summary(wf['id'], invoke_response['id'])
-        report = self.gi.workflows.get_invocation_report(wf['id'], invoke_response['id'])
+        step_id =  invocation['steps'][0]['id']
+
+        self.assertIsNone(
+            self.gi.invocations.show_invocation_step(invocation_id, step_id)["action"])
+        self.gi.invocations.run_invocation_step_action(invocation_id, step_id, action=True)
+        self.assertTrue(self.gi.invocations.show_invocation_step(invocation_id, step_id)["action"])
+
+        summary = self.gi.invocations.get_invocation_summary(invocation_id)
+        report = self.gi.invocations.get_invocation_report(invocation_id)
 
         assert invoke_response['state'] == 'new', invoke_response
         assert summary['states'] == {'ok': 1}
