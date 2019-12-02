0a95aa324911fe7c7316476a4c7cd3853da3cf5a
Simon Bray <sbray1371@gmail.com>
2019-12-02 14:29:37 +0100

    add copy_dataset function

--- bioblend/galaxy/histories/__init__.py
+++ bioblend/galaxy/histories/__init__.py
@@ -660,3 +660,26 @@ class HistoryClient(Client):
         r.raise_for_status()
         for chunk in r.iter_content(chunk_size):
             outf.write(chunk)
+
+    def copy_dataset(self, history_id, dataset_id, hda_ldda='hda'):
+        """
+        Add a dataset to a history.
+
+        :type history_id: str
+        :param history_id: history ID
+
+        :type dataset_id: str
+        :param dataset_id: dataset ID
+
+        :rtype: None
+        :return: None
+        """
+
+        payload = {
+            'content': dataset_id,
+            'source': hda_ldda,
+            'type': 'dataset'
+        }
+
+        url = self.gi._make_url(self, history_id, contents=True)
+        return self._post(payload=payload, url=url)
