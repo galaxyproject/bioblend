0f2bce2b67e3cdf530349ca13f7d67b492e4535b
Nolan Woods <nolan@i2labs.ca>
2020-02-03 12:12:09 -0800

    Move 'number_of_steps', 'show_in_tool_panel' attrs to WorkflowPreview

--- bioblend/galaxy/objects/wrappers.py
+++ bioblend/galaxy/objects/wrappers.py
@@ -184,7 +184,7 @@ class Workflow(Wrapper):
     results from an input dataset.
     """
     BASE_ATTRS = Wrapper.BASE_ATTRS + (
-        'deleted', 'inputs', 'published', 'steps', 'tags', 'owner', 'number_of_steps', 'show_in_tool_panel', 'latest_workflow_uuid'
+        'deleted', 'inputs', 'published', 'steps', 'tags', 'owner', 'latest_workflow_uuid'
     )
     POLLING_INTERVAL = 10  # for output state monitoring
 
@@ -1453,7 +1453,7 @@ class WorkflowPreview(Preview):
     Instances of this class wrap dictionaries obtained by getting
     ``/api/workflows`` from Galaxy.
     """
-    BASE_ATTRS = Preview.BASE_ATTRS + ('published', 'tags')
+    BASE_ATTRS = Preview.BASE_ATTRS + ('published', 'tags', 'number_of_steps', 'show_in_tool_panel')
 
     def __init__(self, pw_dict, gi=None):
         super().__init__(pw_dict, gi=gi)
