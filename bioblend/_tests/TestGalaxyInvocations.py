48417932732746dd9da9171dafc0c92aa9052089
rikeshi <rikeshi@users.noreply.github.com>
2021-02-12 16:30:26 +0100

    add _wait_invocation function

--- bioblend/_tests/TestGalaxyInvocations.py
+++ bioblend/_tests/TestGalaxyInvocations.py
@@ -29,13 +29,13 @@ class TestGalaxyInvocations(GalaxyTestBase.GalaxyTestBase):
         report = self.gi.invocations.get_invocation_report(invocation_id)
         assert report['workflows'] == {workflow_id: {'name': 'paste_columns'}}
         with self.assertRaises(Exception):
-            self.gi.invocations.get_invocation_report(invocation_id, format='pdf')
+            self.gi.invocations.get_invocation_report_pdf(invocation_id)
 
     @test_util.skip_unless_galaxy('release_20.09')
     def test_get_invocation_biocompute_object(self):
         invocation = self._invoke_workflow()
 
-        time.sleep(3)
+        _steps = self._wait_invocation(invocation['id'])
         biocompute_object = self.gi.invocations.get_invocation_biocompute_object(invocation['id'])
         self.assertEqual(len(biocompute_object['description_domain']['pipeline_steps']), 1)
 
@@ -43,7 +43,7 @@ class TestGalaxyInvocations(GalaxyTestBase.GalaxyTestBase):
     def test_get_invocation_step_jobs_summary(self):
         invocation = self._invoke_workflow()
 
-        time.sleep(3)
+        _steps = self._wait_invocation(invocation['id'])
         step_jobs_summary = self.gi.invocations.get_invocation_step_jobs_summary(invocation['id'])
         self.assertEqual(len(step_jobs_summary), 1)
         self.assertEqual(step_jobs_summary[0]['populated_state'], 'ok')
@@ -63,16 +63,8 @@ class TestGalaxyInvocations(GalaxyTestBase.GalaxyTestBase):
         )
         invocation_id = invocation["id"]
 
-        def invocation_steps_by_order_index():
-            invocation = self.gi.invocations.show_invocation(invocation_id)
-            return {s["order_index"]: s for s in invocation["steps"]}
+        steps = self._wait_invocation(invocation_id)
 
-        for _ in range(20):
-            if 2 in invocation_steps_by_order_index():
-                break
-            time.sleep(.5)
-
-        steps = invocation_steps_by_order_index()
         pause_step = steps[2]
         self.assertIsNone(
             self.gi.invocations.show_invocation_step(invocation_id, pause_step["id"])["action"])
@@ -101,3 +93,15 @@ class TestGalaxyInvocations(GalaxyTestBase.GalaxyTestBase):
             history_id=history_id,
             inputs_by='name',
         )
+
+    def _wait_invocation(self, invocation_id):
+        def invocation_steps_by_order_index():
+            invocation = self.gi.invocations.show_invocation(invocation_id)
+            return {s["order_index"]: s for s in invocation["steps"]}
+
+        for _ in range(20):
+            if 2 in invocation_steps_by_order_index():
+                break
+            time.sleep(.5)
+
+        return invocation_steps_by_order_index()
