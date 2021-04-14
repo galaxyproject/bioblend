81a128097054a8257cb2f0b11acfa79f235c2ba4
rikeshi <rikeshi@users.noreply.github.com>
2021-04-14 18:35:51 +0200

    expand test_get_datasets

--- bioblend/_tests/TestGalaxyDatasets.py
+++ bioblend/_tests/TestGalaxyDatasets.py
@@ -61,6 +61,63 @@ class TestGalaxyDatasets(GalaxyTestBase.GalaxyTestBase):
         self.assertEqual(datasets, [])
         datasets = self.gi.datasets.get_datasets(history_id=self.history_id)
         self.assertEqual(len(datasets), 1)
+        datasets = self.gi.datasets.get_datasets(history_id=self.history_id, offset=1)
+        self.assertEqual(datasets, [])
+
+        datasets = self.gi.datasets.get_datasets(history_id=self.history_id, name='Pasted Entry')
+        self.assertEqual(len(datasets), 1)
+        datasets = self.gi.datasets.get_datasets(history_id=self.history_id, name='Wrong Name')
+        self.assertEqual(datasets, [])
+
+        dataset = self.gi.datasets.wait_for_dataset(self.dataset_id)
+        ct = dataset['create_time']
+        datasets = self.gi.datasets.get_datasets(history_id=self.history_id, create_time={'gt': ct})
+        self.assertEqual(datasets, [])
+        datasets = self.gi.datasets.get_datasets(history_id=self.history_id, create_time={'ge': ct})
+        self.assertEqual(len(datasets), 1)
+        datasets = self.gi.datasets.get_datasets(history_id=self.history_id, create_time={'lt': ct})
+        self.assertEqual(datasets, [])
+        datasets = self.gi.datasets.get_datasets(history_id=self.history_id, create_time={'le': ct})
+        self.assertEqual(len(datasets), 1)
+
+        ut = dataset['update_time']
+        datasets = self.gi.datasets.get_datasets(history_id=self.history_id, update_time={'gt': ut})
+        self.assertEqual(datasets, [])
+        datasets = self.gi.datasets.get_datasets(history_id=self.history_id, update_time={'ge': ut})
+        self.assertEqual(len(datasets), 1)
+        datasets = self.gi.datasets.get_datasets(history_id=self.history_id, update_time={'lt': ut})
+        self.assertEqual(datasets, [])
+        datasets = self.gi.datasets.get_datasets(history_id=self.history_id, update_time={'le': ut})
+        self.assertEqual(len(datasets), 1)
+
+        datasets = self.gi.datasets.get_datasets(history_id=self.history_id, extension='txt')
+        self.assertEqual(len(datasets), 1)
+        datasets = self.gi.datasets.get_datasets(history_id=self.history_id, extension='auto')
+        self.assertEqual(datasets, [])
+
+        datasets = self.gi.datasets.get_datasets(history_id=self.history_id, state='ok')
+        self.assertEqual(len(datasets), 1)
+        datasets = self.gi.datasets.get_datasets(history_id=self.history_id, state='queued')
+        self.assertEqual(datasets, [])
+        with self.assertRaises(ConnectionError):
+            datasets = self.gi.datasets.get_datasets(history_id=self.history_id, state='invalid state name')
+        datasets = self.gi.datasets.get_datasets(history_id=self.history_id, state=['ok', 'queued'])
+        self.assertEqual(len(datasets), 1)
+
+        self.gi.histories.update_dataset(history_id=self.history_id, dataset_id=self.dataset_id, visible=False)
+        datasets = self.gi.datasets.get_datasets(history_id=self.history_id)
+        self.assertEqual(datasets, [])
+        datasets = self.gi.datasets.get_datasets(history_id=self.history_id, visible=False)
+        self.assertEqual(len(datasets), 1)
+        self.gi.histories.update_dataset(history_id=self.history_id, dataset_id=self.dataset_id, visible=True)
+
+        datasets = self.gi.datasets.get_datasets(id=[self.dataset_id])
+        self.assertGreaterEqual(len(datasets), 1)
+        datasets = self.gi.datasets.get_datasets(id=['invalid dataset id'])
+        self.assertEqual(datasets, [])
+        # This does not seem to work, returns []
+        # datasets = self.gi.datasets.get_datasets(history_id=self.history_id, id=[self.dataset_id])
+        # self.assertEqual(len(datasets), 1)
 
     def test_wait_for_dataset(self):
         dataset = self.gi.datasets.wait_for_dataset(self.dataset_id)
