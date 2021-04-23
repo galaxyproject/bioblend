49923c4f9d5060dc84d267041268f9e365180ff6
Simon Bray <32272674+simonbray@users.noreply.github.com>
2021-04-23 11:13:58 +0200

    modify get_citations test

--- bioblend/_tests/TestGalaxyTools.py
+++ bioblend/_tests/TestGalaxyTools.py
@@ -146,8 +146,8 @@ class TestGalaxyTools(GalaxyTestBase.GalaxyTestBase):
     def test_get_citations(self):
         citations = self.gi.tools.get_citations('sra_source')
         self.assertEqual(len(citations), 2)
-        self.assertTrue(citations[0]['content'].startswith('@article{Blankenberg_2011,'))
-        self.assertTrue(citations[1]['content'].startswith('@article{Kent_2002,'))
+        self.assertTrue('Blankenberg_2011' in citations[0]['content'])
+        self.assertTrue('Kent_2002' in citations[1]['content'])
 
     def test_tool_dependency_uninstall(self):
         status = self.gi.tools.uninstall_dependencies('CONVERTER_fasta_to_bowtie_color_index')
