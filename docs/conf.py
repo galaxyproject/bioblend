d210c0c5da89cd51d2f6ed4228b6239a0c895aaa
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2023-04-25 17:41:13 +0100

    Fix missing bullet points with old sphinx-rtd-theme
    Also use the theme when building docs locally for easier testing.

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ from bioblend import get_version  # noqa: E402
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.todo", "sphinx.ext.coverage", "sphinx.ext.viewcode"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.todo", "sphinx.ext.coverage", "sphinx.ext.viewcode", "sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -102,7 +102,7 @@ autoclass_content = "both"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "default"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
