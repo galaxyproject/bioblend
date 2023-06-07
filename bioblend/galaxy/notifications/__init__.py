ecf20b5098958b7c3db6c4f0ecc870c60a618c9e
Tillman <theisner@posteo.de>
2023-06-07 13:54:42 +0200

    Refactor get_notification_status operation for easier use

--- bioblend/galaxy/notifications/__init__.py
+++ bioblend/galaxy/notifications/__init__.py
@@ -7,6 +7,7 @@ from typing import (
     Any,
     Dict,
     List,
+    Optional,
     TYPE_CHECKING,
     Union,
 )
@@ -23,7 +24,9 @@ class NotificationClient(Client):
     def __init__(self, galaxy_instance: "GalaxyInstance") -> None:
         super().__init__(galaxy_instance)
 
-    def get_notification_status(self, since: datetime) -> Dict[str, Union[int, List[Any]]]:
+    def get_notification_status(
+        self, year: int, month: int, day: int, hour: int, minute: Optional[int] = None, second: Optional[int] = None
+    ) -> Dict[str, Union[int, List[Any]]]:
         """
         Fetches the current status summary of the user's notifications
         since a particular date.
@@ -31,8 +34,34 @@ class NotificationClient(Client):
         :type since: datetime
         :param since: Retrieval of the notifications starts from this point in time
 
+        :type year: int
+        :param year: Retrieval of the notifications starts from this year
+
+        :type month: int
+        :param month: Retrieval of the notifications starts from this month
+
+        :type day: int
+        :param day: Retrieval of the notifications starts from this day
+
+        :type hour: int
+        :param hour: Retrieval of the notifications starts from this hour
+
+        :type minute: int
+        :param minute: Retrieval of the notifications starts from this minute. Defaults to 0
+
+        :type second: int
+        :param second: Retrieval of the notifications starts from this second. Defaults to 0
+
         :return: A dictionary containing the current status summary of notifications.
         :rtype: dict
         """
+        minute = minute or 0
+        second = second or 0
+        try:
+            # Construct the datetime object based on the provided inputs
+            since = datetime(year, month, day, hour, minute, second)
+        except ValueError as e:
+            # Handle the conversion error
+            raise ValueError("Invalid date and time inputs.") from e
         url = self._make_url() + f"/status?since={since}"
         return self._get(url=url)
