6afa981ace4f1f2266c13b0dd1efa292e582c1f8
Tillman <theisner@posteo.de>
2023-06-07 13:59:07 +0200

    Refactor test for get_notification_status operation for a more comprehensive test

--- bioblend/_tests/TestGalaxyNotifications.py
+++ bioblend/_tests/TestGalaxyNotifications.py
@@ -1,11 +1,17 @@
 from datetime import datetime
 
+import pytest
+
 from . import GalaxyTestBase
 
 
 class TestGalaxyNotifications(GalaxyTestBase.GalaxyTestBase):
     def test_empty_notification_status(self):
-        status = self.gi.notifications.get_notification_status(datetime.utcnow())
+        year = datetime.utcnow().year
+        month = datetime.utcnow().month
+        day = datetime.utcnow().day
+        hour = datetime.utcnow().hour
+        status = self.gi.notifications.get_notification_status(year=year, month=month, day=day, hour=hour)
         # check the datatype
         assert type(status["total_unread_count"]) == int
         assert type(status["broadcasts"]) == list
@@ -14,3 +20,8 @@ class TestGalaxyNotifications(GalaxyTestBase.GalaxyTestBase):
         assert status["total_unread_count"] == 0
         assert status["broadcasts"] == []
         assert status["notifications"] == []
+        # check if incorrect gets processed correctly
+        with pytest.raises(ValueError) as error_info:
+            self.gi.notifications.get_notification_status(year=year, month=13, day=day, hour=hour)
+        assert error_info.type == ValueError
+        assert str(error_info.value) == "Invalid date and time inputs."
