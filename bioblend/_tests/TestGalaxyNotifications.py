b824c477ceb580b8c3178a650588f16b88a53a28
Tillman <theisner@posteo.de>
2023-06-07 13:09:13 +0200

    Refactor test for get_notification_status operation

--- bioblend/_tests/TestGalaxyNotifications.py
+++ bioblend/_tests/TestGalaxyNotifications.py
@@ -10,3 +10,7 @@ class TestGalaxyNotifications(GalaxyTestBase.GalaxyTestBase):
         assert type(status["total_unread_count"]) == int
         assert type(status["broadcasts"]) == list
         assert type(status["notifications"]) == list
+        # check if the notification is really empty
+        assert status["total_unread_count"] == 0
+        assert status["broadcasts"] == []
+        assert status["notifications"] == []
