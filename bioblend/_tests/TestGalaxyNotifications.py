79a1c29b6a6c461ea48db951bfee0edf9428675b
Tillman <theisner@posteo.de>
2023-06-27 13:51:40 +0200

    Refactor test for get_notifications_status

--- bioblend/_tests/TestGalaxyNotifications.py
+++ bioblend/_tests/TestGalaxyNotifications.py
@@ -1,27 +1,27 @@
 from datetime import datetime
 
-import pytest
-
-from . import GalaxyTestBase
+from . import (
+    GalaxyTestBase,
+    test_util,
+)
 
 
 class TestGalaxyNotifications(GalaxyTestBase.GalaxyTestBase):
+    @test_util.skip_unless_galaxy("release_23.1")
     def test_empty_notification_status(self):
-        year = datetime.utcnow().year
-        month = datetime.utcnow().month
-        day = datetime.utcnow().day
-        hour = datetime.utcnow().hour
-        status = self.gi.notifications.get_notification_status(year=year, month=month, day=day, hour=hour)
+        if not self.gi.config.get_config()["enable_notification_system"]:
+            self.skipTest("This Galaxy instance is not configured to use notifications.")
+
+        # fetch the empty notification status
+        now = datetime.utcnow()
+        status = self.gi.notifications.get_notification_status(since=now)
+
         # check the datatype
         assert type(status["total_unread_count"]) == int
         assert type(status["broadcasts"]) == list
         assert type(status["notifications"]) == list
+
         # check if the notification is really empty
         assert status["total_unread_count"] == 0
         assert status["broadcasts"] == []
         assert status["notifications"] == []
-        # check if incorrect gets processed correctly
-        with pytest.raises(ValueError) as error_info:
-            self.gi.notifications.get_notification_status(year=year, month=13, day=day, hour=hour)
-        assert error_info.type == ValueError
-        assert str(error_info.value) == "Invalid date and time inputs."
