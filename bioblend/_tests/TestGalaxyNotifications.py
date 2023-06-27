8f9eb5236562f5ecc665e80505959fc93c1314a4
Tillman <theisner@posteo.de>
2023-06-27 14:00:16 +0200

    Add test for get_notification_preferences and update_notification_preferences operation

--- bioblend/_tests/TestGalaxyNotifications.py
+++ bioblend/_tests/TestGalaxyNotifications.py
@@ -25,3 +25,38 @@ class TestGalaxyNotifications(GalaxyTestBase.GalaxyTestBase):
         assert status["total_unread_count"] == 0
         assert status["broadcasts"] == []
         assert status["notifications"] == []
+
+    @test_util.skip_unless_galaxy("release_23.1")
+    def test_notification_preferences(self):
+        if not self.gi.config.get_config()["enable_notification_system"]:
+            self.skipTest("This Galaxy instance is not configured to use notifications.")
+
+        # the notification preferences, which should be set to true by default
+        preferences = self.gi.notifications.get_notification_preferences()
+
+        # notifications preferences for messages
+        message_preferences = preferences["preferences"]["message"]
+        assert message_preferences["enabled"] is True
+        assert message_preferences["channels"]["push"] is True
+
+        # notifications preferences for new shared items
+        new_items_preferences = preferences["preferences"]["new_shared_item"]
+        assert new_items_preferences["enabled"] is True
+        assert new_items_preferences["channels"]["push"] is True
+
+        # change preferences to False
+        self.gi.notifications.update_notification_preferences(
+            message_notifications=False,
+            push_notifications_message=False,
+            new_item_notifications=False,
+            push_notifications_new_items=False,
+        )
+
+        # check the updated notification preferences
+        preferences = self.gi.notifications.get_notification_preferences()
+        message_preferences = preferences["preferences"]["message"]
+        assert message_preferences["enabled"] is False
+        assert message_preferences["channels"]["push"] is False
+        new_items_preferences = preferences["preferences"]["new_shared_item"]
+        assert new_items_preferences["enabled"] is False
+        assert new_items_preferences["channels"]["push"] is False
