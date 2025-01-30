f5a26d67812cb0ea74599358e063eaa9a3bd93ed
M Bernt <m.bernt@ufz.de>
2025-01-30 18:32:38 +0100

    Simply test subfolder creation
    Co-authored-by: Nicola Soranzo <nicola.soranzo@gmail.com>

--- bioblend/_tests/TestGalaxyFolders.py
+++ bioblend/_tests/TestGalaxyFolders.py
@@ -41,9 +41,7 @@ class TestGalaxyFolders(GalaxyTestBase.GalaxyTestBase):
 
     @test_util.skip_unless_galaxy("release_21.05")
     def test_show_folder_contents_limit(self):
-        subfolders = []
-        for i in range(12):
-            subfolders.append(self.gi.folders.create_folder(self.folder["id"], f"{self.name} {i}"))
+        subfolders = [self.gi.folders.create_folder(self.folder["id"], f"{self.name} {i}") for i in range(12)]
 
         # check defaults for limit and offset
         f2 = self.gi.folders.show_folder(self.folder["id"], contents=True)
