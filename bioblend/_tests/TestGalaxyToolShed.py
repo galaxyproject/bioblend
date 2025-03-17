18a38c17e4c4f87cf9a99ea1e8bc4cda71af203f
Matthias Bernt <m.bernt@ufz.de>
2025-03-17 18:04:20 +0100

    fix linter errors

--- bioblend/_tests/TestGalaxyToolShed.py
+++ bioblend/_tests/TestGalaxyToolShed.py
@@ -12,10 +12,10 @@ class TestGalaxyToolShed(GalaxyTestBase.GalaxyTestBase):
         # asking to install old version immediately installs
         # new version
         response = self.gi.toolshed.install_repository_revision(
-            tool_shed_url = "https://toolshed.g2.bx.psu.edu/",
-            name = "ampvis2_alpha_diversity",
-            owner = "iuc",
-            changeset_revision = "289d6299bd2e",
+            tool_shed_url="https://toolshed.g2.bx.psu.edu/",
+            name="ampvis2_alpha_diversity",
+            owner="iuc",
+            changeset_revision="289d6299bd2e",
         )
         assert isinstance(response, list), str(response)
         assert len(response) == 1
@@ -27,21 +27,21 @@ class TestGalaxyToolShed(GalaxyTestBase.GalaxyTestBase):
         assert installed_repos[0]['changeset_revision'] == 'c14c7fd4d1be'
 
         self.gi.toolshed.uninstall_repository_revision(
-            name = "ampvis2_alpha_diversity",
-            owner = "iuc",
-            changeset_revision = "c14c7fd4d1be",
-            tool_shed_url = "https://toolshed.g2.bx.psu.edu/",
-            remove_from_disk = True,
+            name="ampvis2_alpha_diversity",
+            owner="iuc",
+            changeset_revision="c14c7fd4d1be",
+            tool_shed_url="https://toolshed.g2.bx.psu.edu/",
+            remove_from_disk=True,
         )
 
     def test_install_new_first(self):
         # 6:289d6299bd2e
         # 7:c14c7fd4d1be has not been bumped
         response = self.gi.toolshed.install_repository_revision(
-            tool_shed_url = "https://toolshed.g2.bx.psu.edu/",
-            name = "ampvis2_alpha_diversity",
-            owner = "iuc",
-            changeset_revision = "c14c7fd4d1be",
+            tool_shed_url="https://toolshed.g2.bx.psu.edu/",
+            name="ampvis2_alpha_diversity",
+            owner="iuc",
+            changeset_revision="c14c7fd4d1be",
         )
         assert isinstance(response, list), str(response)
         assert len(response) == 1
@@ -56,10 +56,10 @@ class TestGalaxyToolShed(GalaxyTestBase.GalaxyTestBase):
         # -> galaxy realizes that a tool with the same version is alredy installed
         # -> responds with a dict indicating this
         response = self.gi.toolshed.install_repository_revision(
-            tool_shed_url = "https://toolshed.g2.bx.psu.edu/",
-            name = "ampvis2_alpha_diversity",
-            owner = "iuc",
-            changeset_revision = "289d6299bd2e",
+            tool_shed_url="https://toolshed.g2.bx.psu.edu/",
+            name="ampvis2_alpha_diversity",
+            owner="iuc",
+            changeset_revision="289d6299bd2e",
         )
         assert isinstance(response, dict), str(response)
         assert response["status"] == 'ok'
@@ -67,12 +67,12 @@ class TestGalaxyToolShed(GalaxyTestBase.GalaxyTestBase):
         installed_repos = self.gi.toolshed.get_repositories()
         assert len(installed_repos) == 1
         assert installed_repos[0]['installed_changeset_revision'] == 'c14c7fd4d1be'
-        assert installed_repos[0]['changeset_revision'] == 'c14c7fd4d1be'            
+        assert installed_repos[0]['changeset_revision'] == 'c14c7fd4d1be'
 
         self.gi.toolshed.uninstall_repository_revision(
-            name = "ampvis2_alpha_diversity",
-            owner = "iuc",
-            changeset_revision = "c14c7fd4d1be",
-            tool_shed_url = "https://toolshed.g2.bx.psu.edu/",
-            remove_from_disk = True,
+            name="ampvis2_alpha_diversity",
+            owner="iuc",
+            changeset_revision="c14c7fd4d1be",
+            tool_shed_url="https://toolshed.g2.bx.psu.edu/",
+            remove_from_disk=True,
         )
